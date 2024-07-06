c4f0e53d58e1a7b6f7693095a64999bfb7e88f6b
SiriVarshiniKarri <sirivarshinikarri@gmail.com>
2024-07-06 09:30:23 +0530

    Third

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "What is 15+15 ?",
+            "Who invented Telephone?",
+            "What is 12*7?",
+            "Who is the founder of Tesla?",
+            "In the given options,Which is the Example of System Software?"
+    };
+
+    public static String choices[][] = {
+            {"37", "30", "45", "42"},
+            {"Einstein", "Charles Babbage", "Graham Bell", "None of the above"},
+            {"91", "87", "92", "84"},
+            {"Elon Musk", "Mark Zuckerberg", "Ratan Tata", "Bill Gates"},
+            {"Linux", "MacOS", "Windows", "All of the above"}
+    };
+
+    public static String correctAnswers[] = {
+            "30",
+            "Graham Bell",
+            "84",
+            "Elon Musk",
+            "All of the above"
+    };
+}
